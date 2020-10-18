349db0fa3f7255a3169fa6454f82b4d37923cf35
Allan Callaghan <calaldees@gmail.com>
2020-10-18 17:35:34 +0100

    missing file

---
+++ thing.py
@@ -0,0 +1,17 @@
+import requests
+import numpy
+
+
+def method_that_is_cool(a, b):
+    """
+    >>> method_that_is_cool(1, 2)
+    None
+    >>> method_that_is_cool(2, 3)
+    None
+    """
+    return None
+
+
+if __name__ == '__main__':
+    print('Hello World')
+    method_that_is_cool(4, 5)
